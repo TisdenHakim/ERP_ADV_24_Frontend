e48a2ae6e60494b49d44043e596bcaa56900e6e7
Hakim Ghanoudi <hakim.ghanoudi@student.ap.be>
2024-06-19 10:17:05 +0200

    Add shell bar column with movements

--- movementsmodule/webapp/controller/Movements.controller.js
+++ movementsmodule/webapp/controller/Movements.controller.js
@@ -1,15 +1,28 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/format/DateFormat", // Add DateFormat module
+  ],
+  function (Controller, DateFormat) {
+    "use strict";
 
-        return Controller.extend("movementsmodule.controller.Movements", {
-            onInit: function () {
+    return Controller.extend("movementsmodule.controller.Movements", {
+      onInit: function () {
+        // Initialization logic can be added here if needed
+      },
 
-            }
+      formatDate: function (dateString) {
+        if (!dateString) {
+          return "";
+        }
+        var oDateFormat = DateFormat.getDateInstance({
+          pattern: "dd.MM.yyyy", // Adjust the date pattern as per your requirement
         });
+        var oDate = new Date(dateString);
+        return oDateFormat.format(oDate);
+      },
+
+      // Define other controller methods as needed
     });
+  }
+);
